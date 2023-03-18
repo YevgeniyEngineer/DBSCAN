6c0a13d7f69a572b398f5b69835c240739e6098f
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-03-18 17:02:03 +0800

    Basic DBSCAN implementation using brute force nearest neighbour search, threaded

---
+++ include/dbscan.hpp
@@ -0,0 +1,116 @@
+#ifndef DBSCAN_HPP
+#define DBSCAN_HPP
+
+#include <algorithm>
+#include <cmath>
+#include <eigen3/Eigen/Dense>
+#include <mutex>
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+#include <tbb/queuing_mutex.h>
+#include <vector>
+
+namespace clustering
+{
+using namespace std;
+using namespace Eigen;
+using namespace tbb;
+
+typedef Eigen::Matrix<double, Eigen::Dynamic, 3> PointCloud;
+
+class DBSCAN
+{
+  public:
+    explicit DBSCAN(double eps, int min_pts, const PointCloud &points) : eps_(eps), min_pts_(min_pts), points_(points)
+    {
+        cluster_labels_.resize(points_.rows(), -1);
+    }
+
+    void run()
+    {
+        int cluster_id = 0;
+
+        tbb::parallel_for(tbb::blocked_range<int>(0, points_.rows()), [&](const tbb::blocked_range<int> &r) {
+            for (int i = r.begin(); i != r.end(); ++i)
+            {
+                if (cluster_labels_[i] != -1)
+                {
+                    continue;
+                }
+                std::vector<int> neighbors = regionQuery(i);
+                if (neighbors.size() < min_pts_)
+                {
+                    std::unique_lock<std::mutex> lock(mutex_);
+                    cluster_labels_[i] = 0; // Noise
+                }
+                else
+                {
+                    std::unique_lock<std::mutex> lock(mutex_);
+                    expandCluster(i, neighbors, ++cluster_id);
+                }
+            }
+        });
+    }
+
+    double eps() const
+    {
+        return eps_;
+    }
+
+    int min_pts() const
+    {
+        return min_pts_;
+    }
+
+    std::vector<int> cluster_labels() const
+    {
+        return cluster_labels_;
+    }
+
+  private:
+    void expandCluster(int point_idx, std::vector<int> &neighbors, int cluster_id)
+    {
+        cluster_labels_[point_idx] = cluster_id;
+
+        for (size_t i = 0; i < neighbors.size(); ++i)
+        {
+            int n = neighbors[i];
+            if (cluster_labels_[n] == -1)
+            {
+                cluster_labels_[n] = cluster_id;
+                std::vector<int> n_neighbors = regionQuery(n);
+                if (n_neighbors.size() >= min_pts_)
+                {
+                    neighbors.insert(neighbors.end(), n_neighbors.begin(), n_neighbors.end());
+                }
+            }
+            else if (cluster_labels_[n] == 0)
+            {
+                cluster_labels_[n] = cluster_id;
+            }
+        }
+    }
+
+    std::vector<int> regionQuery(int point_idx)
+    {
+        std::vector<int> neighbors;
+        for (int i = 0; i < points_.rows(); ++i)
+        {
+            if (i != point_idx && (points_.row(i) - points_.row(point_idx)).norm() <= eps_)
+            {
+                neighbors.push_back(i);
+            }
+        }
+        return neighbors;
+    }
+
+    double eps_;
+    int min_pts_;
+    PointCloud points_;
+    std::vector<int> cluster_labels_;
+    std::mutex mutex_;
+};
+
+} // namespace clustering
+
+#endif // DBSCAN_HPP
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include "dbscan.hpp"
+#include <chrono>
+#include <iostream>
+#include <random>
+
+int main()
+{
+    using namespace clustering;
+
+    int number_of_points = 100'000;
+    int number_of_dimensions = 3;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    PointCloud points(number_of_points, number_of_dimensions);
+    for (auto i = 0; i < number_of_points; ++i)
+    {
+        double x = dist(gen);
+        double y = dist(gen);
+        double z = dist(gen);
+        Eigen::Vector3d point(x, y, z);
+        points.row(i) = point;
+    }
+
+    double eps = 0.1;
+    int min_pts = 2;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    DBSCAN dbscan(eps, min_pts, points);
+    dbscan.run();
+
+    auto stop_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time: " << (stop_time - start_time).count() / 1e9 << " seconds" << std::endl;
+
+    // for (const int label : dbscan.cluster_labels())
+    // {
+    //     cout << label << endl;
+    // }
+
+    return EXIT_SUCCESS;
+}
